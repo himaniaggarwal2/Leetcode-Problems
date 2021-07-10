ffa668716e6d1872b24f75b87987fe13d7269615
himaniaggarwal2 <himaniaggarwal2@gmail.com>
2021-07-10 20:03:57 +0530

    Updated code

--- breadthFirstSearch.cpp
+++ breadthFirstSearch.cpp
@@ -1,26 +1,29 @@
 #include <stdc++.h>
 using namespace std;
 
-template <typename T>
-
 class Graph{
-    map<T,list<T> > l;
+    int v;
+    list<int> *l;
 
 public:
+    Graph(int V){
+        this->v=V;
+        l= new list<int> [V];
+    }
     void addEdge(int x,int y){
         l[x].push_back(y);
         l[y].push_back(x);
     }
 
-    void bfs(T, int src){
-        map<T, int> visited;
-        queue<T> q;
+    void bfs(int src){
+        bool *visited =  new bool[v] {0};
+        queue<int> q;
 
         q.push(src);
         visited[src]=true;
 
         while(!q.empty()){
-            T node = q.front();
+            int node = q.front();
             q.pop();
 
             for(int neighbor: l[node]){
@@ -31,4 +34,19 @@ public:
             }
         }
     }
+};
+
+int main(){
+
+    Graph g(7);
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(5,6);
+    g.addEdge(4,5);
+    g.addEdge(0,4);
+    g.addEdge(3,4);
+    g.addEdge(3,5);
+
+    g.bfs(1);
+    return 0;
 }
