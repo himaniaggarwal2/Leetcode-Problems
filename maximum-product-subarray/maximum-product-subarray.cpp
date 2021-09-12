abf819fe6fa51f28a3a4398cdfc2d2c29258a47c
himaniaggarwal2 <47392245+himaniaggarwal2@users.noreply.github.com>
2021-09-12 13:26:50 +0530

    Time: 3 ms (86.69%), Space: 11.7 MB (54.42%) - LeetHub

--- maximum-product-subarray/maximum-product-subarray.cpp
+++ maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int prev_max_product=nums[0];
-        int n=nums.size();
+        int prev_max=nums[0],prev_min=nums[0],curr_max,curr_min;
         int result=nums[0];
-        int prev_min_product=nums[0];
-        int curr_min_product,curr_max_product;
+        int n=nums.size();
         for(int i=1;i<n;i++){
-            curr_max_product=max({prev_min_product*nums[i],prev_max_product*nums[i],nums[i]});
-            curr_min_product=min({prev_min_product*nums[i],prev_max_product*nums[i],nums[i]});
-            result=max(curr_max_product,result);
-            prev_min_product=curr_min_product;
-            prev_max_product=curr_max_product;
+            curr_min=std::min({prev_min*nums[i],prev_max*nums[i],nums[i]});
+            curr_max=std::max({prev_min*nums[i],prev_max*nums[i],nums[i]});
+            result=std::max(curr_max,result);
+            prev_min=curr_min;
+            prev_max=curr_max;
         }
         return result;
     }
