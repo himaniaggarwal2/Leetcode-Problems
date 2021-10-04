0c9835c73cce6f479b75fd94886cefac899aa12a
himaniaggarwal2 <47392245+himaniaggarwal2@users.noreply.github.com>
2021-10-05 00:26:20 +0530

    Time: 34 ms (21.26%), Space: 23.3 MB (41.75%) - LeetHub

---
+++ reverse-string/reverse-string.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i=0,j=s.size()-1;
+        while(i<=j){
+            std::swap(s[i],s[j]);
+            i++;
+            j--;
+        }
+    }
+};
