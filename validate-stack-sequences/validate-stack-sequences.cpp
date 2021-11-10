dd3498d90b7ce2a42f2055bb99a136afde0d17d7
himaniaggarwal2 <47392245+himaniaggarwal2@users.noreply.github.com>
2021-11-11 00:09:28 +0530

    Time: 8 ms (78.88%), Space: 15.4 MB (15.29%) - LeetHub

---
+++ validate-stack-sequences/validate-stack-sequences.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        int j=0;
+        for(int e:pushed){
+            st.push(e);
+            while(st.size()>0 and st.top()==popped[j]){
+                st.pop();
+                j++;
+            }
+        }
+        return j==popped.size();
+    }
+};
