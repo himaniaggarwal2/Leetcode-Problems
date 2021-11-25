df7c77c926af33fd88cdeefe8f15a463874d32a3
himaniaggarwal2 <47392245+himaniaggarwal2@users.noreply.github.com>
2021-11-25 20:48:53 +0530

    Time: 3 ms (61.20%), Space: 7.5 MB (33.74%) - LeetHub

--- subsets/subsets.cpp
+++ subsets/subsets.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    vector <vector <int>> powerSet;
-    void getSubset(vector<int> &nums, vector<int> subset, int i) {
-        if (i == nums.size()) {
-            powerSet.push_back(subset);
-            return;
-        }
-        
-        // accepting element
-        subset.push_back(nums[i]);
-        getSubset(nums, subset, i + 1);
-        
-        // rejecting element
-        subset.pop_back();
-        getSubset(nums, subset, i + 1);
-    }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector <int> subset;
-        getSubset(nums, subset, 0);
-        return powerSet;
+        vector<vector<int>> list;
+        std::sort(nums.begin(),nums.end());
+        backtrack(list,vector<int> {},nums,0);
+        return list;
+    }
+    void backtrack(vector<vector<int>> &list, vector<int> temp, vector<int> nums, int start){
+        list.push_back(temp);
+        for(int i=start;i<nums.size();i++){
+            temp.push_back(nums[i]);
+            backtrack(list,temp,nums,i+1);
+            int n=temp.size();
+            temp.pop_back();
+        }
     }
 };
+
