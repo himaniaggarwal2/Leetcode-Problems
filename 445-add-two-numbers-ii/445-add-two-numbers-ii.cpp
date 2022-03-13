f175c364f2861f4914a84e14362856180bbd33b2
himaniaggarwal2 <47392245+himaniaggarwal2@users.noreply.github.com>
2022-03-13 13:57:44 +0530

    Time: 46 ms (63.83%), Space: 71.4 MB (44.40%) - LeetHub

---
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode* head){
+        ListNode* prev=nullptr;
+        ListNode* curr=head;
+        ListNode* forw=nullptr;
+        
+        while(curr!=nullptr){
+            forw = curr->next;
+            curr->next = prev;
+            prev=curr;
+            curr=forw;
+        }
+        return prev;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        if(l1==nullptr or l2==nullptr) return l1==nullptr?l2:l1;
+        
+        ListNode* c1= reverse(l1);
+        ListNode* c2= reverse(l2);
+        
+        ListNode* head =  new ListNode(-1);
+        ListNode* itr=head;
+        
+        int carry=0;
+        while(c1!=nullptr or c2!=nullptr or carry!=0){
+            int sum = carry + (c1 != nullptr ? c1->val: 0) + (c2 != nullptr ? c2->val : 0);
+            int ld= sum%10;
+            carry = sum/10;
+            
+            ListNode* temp =  new ListNode (ld);
+            itr->next = temp;
+            itr = itr->next;
+            
+            if(c1 != nullptr) c1 = c1->next;
+            if(c2 != nullptr) c2 = c2->next;
+            
+        }
+        
+        return reverse(head->next);
+    }
+};
